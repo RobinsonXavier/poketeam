cd3ff02c4ddf421d2563942e6a1b341b734c46e2
RobinsonXavier <robinson.webzen@gmail.com>
2023-08-24 11:34:58 -0300

    feat: putPokemonData function on script.js // fix: clickSearchPokemon function on script.js // style: adding new styles for new and old content

--- src/scripts/pokemonRepository.js
+++ src/scripts/pokemonRepository.js
@@ -1,23 +1,29 @@
 export async function getPokemon() {
-  const pokemonName = document.getElementById("pokemon-procurado").value;
-  try {
-    const response = await fetch(`http://localhost:5000/pokemonData`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({pokename: pokemonName}),
-    });
+  const pokemonName = document.getElementById("pokemon-procurado").value.toLowerCase();
+    if(isNaN(pokemonName)) {
+      try {
+        const response = await fetch(`http://localhost:5000/pokemonData`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({pokename: pokemonName}),
+        });
+  
+        if(!response) {
+          console.log(response);
+          return;
+        }
+      
+        const pokedata = await response.json();
+        return pokedata;
+    
+      } catch (error) {
+        console.log(error);
+      }
+    } else { //elaborar uma mensagem de texto mais detalhada depois!!
 
-    if(!response) {
-      console.log(response);
-      return;
+      console.log("Escreva o nome de um pokemon!!");
     }
-
-    const pokedata = await response.json();
-    return pokedata;
-  } catch (error) {
-    console.log(error);
-  }
 
 }
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -20,9 +20,14 @@ function closeCaptureWindow() {
 
 function clickSearchPokemon() {
   const searchForm = document.querySelector(".busca-pokemon form");
-  const searchButton = document.querySelector("button");
+  const searchButton = searchForm.querySelector("button");
+  const captureWindow = document.querySelector(".busca-pokemon");
+  
   searchButton.addEventListener("click",async (event) => {
     event.preventDefault();
+
+    captureWindow.classList.remove("visivel");
+    captureWindow.classList.add("invisivel");
 
     const pokemon = await getPokemon();
 
@@ -31,15 +36,87 @@ function clickSearchPokemon() {
       return;
     }
     console.log(pokemon)
-    const siteContent = document.querySelector(".conteudo");
+    const siteContent = document.querySelector(".pokemon-info");
+    //para evitar problemas de nova busca após buscar um pokemon, criar uma nova funçao com textContent
     siteContent.innerHTML = `
+      <h1>Pokemon:</h1>
       <div class="poke-info">
         <img src="${pokemon.pokedata.sprites.front_default}" alt="${pokemon.pokedata.name}"/>
         <h2>${pokemon.pokedata.name.toUpperCase()}</h2>
       </div>
-    `
+    `;
+
+    putPokemonData(pokemon);
 
   })
+}
+
+function putPokemonData(pokemon) {
+  const newContent = document.querySelector(".poke-info");
+
+  newContent.innerHTML += `
+    <h2>Habilidades:</h2>
+  `;
+
+  for(let i = 0; i < pokemon.pokedata.abilities.length; i++) {
+    newContent.innerHTML += `
+      <div>
+        <span>${pokemon.pokedata.abilities[i].ability.name}</span>
+      </div>
+    `;
+  }
+
+  newContent.innerHTML += `
+    <h2>Items:</h2>
+  `;
+
+  for(let i = 0; i < pokemon.pokedata.held_items.length; i++) {
+    newContent.innerHTML += `
+      <div>
+        <span>${pokemon.pokedata.held_items[i].item.name}</span>
+      </div>
+    `;
+  }
+
+  if(pokemon.pokedata.held_items.length < 1) {
+    newContent.innerHTML += `
+    <div>
+      <span>None</span>
+    </div>
+    `;
+  }
+
+  newContent.innerHTML += `
+    <h2>Movimentos:</h2>
+  `;
+
+    for(let i = 0; i < pokemon.pokedata.moves.length; i++) {
+      newContent.innerHTML += `
+        <div>
+          <span>${pokemon.pokedata.moves[i].move.name}</span>
+          <span>Lv. ${pokemon.pokedata.moves[i].version_group_details[0].level_learned_at}</span>
+          <span>Method: ${pokemon.pokedata.moves[i].version_group_details[0].move_learn_method.name}</span>
+        </div>
+      `;
+    }
+  
+  newContent.innerHTML += `
+    <h2>Status:</h2>
+  `;
+
+    for(let i = 0; i < pokemon.pokedata.stats.length; i++) {
+      newContent.innerHTML += `
+        <div>
+          <span>${pokemon.pokedata.stats[i].stat.name}</span>
+          <span> --- ${pokemon.pokedata.stats[i].base_stat}</span>
+        </div>
+      `;
+    }
+
+  newContent.innerHTML += `
+    <h2>Peso:</h2>
+    <span>${pokemon.pokedata.weight}hg</span>
+  `;
 }
 
 openCaptureWindow();
